4b58574d03ecb2f536f5990b73c55496fec54f7e
Dantech <chukwujidan@gmail.com>
2023-05-06 15:26:10 +0100

    initial commit

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -3,5 +3,4 @@ from django.conf import settings
 from pages.models import CompanyProfile
 
 def company(request):
-    pass
-    # return {'company': CompanyProfile.objects.get(id=settings.COMPANY_ID)}
+    return {'company': CompanyProfile.objects.get(id=settings.COMPANY_ID)}
