473e356824b078ffea16d7f321dc0fa926fb07cb
Dantech <chukwujidan@gmail.com>
2023-05-06 15:23:03 +0100

    initial commit

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -97,7 +97,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pages.context_processors.company'
+                #'pages.context_processors.company'
             ],
         },
     },
