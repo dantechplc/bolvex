fed3cf30b8d74b098297f1b55c45073c865dd200
Dantech <chukwujidan@gmail.com>
2023-05-07 13:45:13 +0100

    new commit

--- accounts/views.py
+++ accounts/views.py
@@ -15,6 +15,8 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.views.generic import TemplateView, RedirectView
 from django.core.files import File
+
+from pages.models import CompanyProfile
 from .forms import *
 from .models import User
 
@@ -78,7 +80,7 @@ def signup(request, *args, **kwargs):
             mail_subject = 'Activate your account.'
             message = render_to_string('accounts/acc_active_email1.html', {
                 'user': user,
-
+                'company': CompanyProfile.objects.get(id=settings.COMPANY_ID),
                 'domain': current_site.domain,
                 'uid': urlsafe_base64_encode(force_bytes(user.pk)),
                 'token': default_token_generator.make_token(user),
