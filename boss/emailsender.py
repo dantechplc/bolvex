18ca4aa32669c92673636a6cba3059c21c3528ed
Dantech <chukwujidan@gmail.com>
2023-09-26 01:26:53 +0100

    added context to verification emails

--- boss/emailsender.py
+++ boss/emailsender.py
@@ -1,17 +1,24 @@
+from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
+
+from pages.models import CompanyProfile
 
 
 class EmailSender:
     """"This class is used to send emails"""
 
     def kyc_verified(*agrs, **kwargs):
+        company = CompanyProfile.objects.get(id=settings.COMPANY_ID)
         email = kwargs.get('email')
         name = kwargs.get('name')
         mail_subject = "VERIFICATION SUCCESSFUL"
         to_email = str(email)
         message1 = render_to_string('boss/verifyemail.html', {
             'name': name,
+            'company_address': company.company_address,
+            'company_phone': company.company_phone,
+            'company_email': company.company_support_email,
         })
         # message1 = message
         email = EmailMultiAlternatives(
