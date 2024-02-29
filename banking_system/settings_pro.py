63295e30f58af88bb0fa5dab409eeefcde818581
Dantech <chukwujidan@gmail.com>
2024-02-29 11:07:50 +0100

    added IP

--- banking_system/settings_pro.py
+++ banking_system/settings_pro.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'po0172$69b@78ps4v^uhfxu6q--8ko7kpp7rbz420s_3w#sir%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', "142.93.175.174", "bolvexcapital.com", "www.bolvexcapital.com"]
+ALLOWED_HOSTS = ['*', "147.182.229.150", "bolvexcapital.com", "www.bolvexcapital.com"]
 
 # Application definition
 
