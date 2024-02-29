fecba66031570326985443a188f60483f4f723a1
Dantech <chukwujidan@gmail.com>
2024-02-29 23:53:10 +0100

    added new IP

--- manage.py
+++ manage.py
@@ -2,13 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+from decouple import config
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_system.settings_pro' if os.environ.get('DJANGO_ENVIRONMENT') == 'production' else 'banking_system.settings_dev')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'banking_system.settings_pro' if config(
+        'DJANGO_ENVIRONMENT') == 'production' else 'banking_system.settings_dev')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
