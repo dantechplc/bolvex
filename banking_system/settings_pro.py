a88e86e7147f7264227f011b98b6c814d0a79013
Dantech <chukwujidan@gmail.com>
2024-03-01 02:10:44 +0100

    updated email provider

--- banking_system/settings_pro.py
+++ banking_system/settings_pro.py
@@ -142,11 +142,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.zoho.com'
-DEFAULT_FROM_EMAIL = 'BolvexCapital<support@bolvexcapital.com>'
+DEFAULT_FROM_EMAIL = 'BolvexCapital<info@bolvexcapital.com>'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'support@bolvexcapital.com'
-EMAIL_HOST_PASSWORD = 'lnM!ot33'
+EMAIL_HOST_USER = 'info@bolvexcapital.com'
+EMAIL_HOST_PASSWORD = 'D@ntech3668'
 LOGIN_REDIRECT_URL = 'transaction:dashboard'
 
 COMPANY_ID = 1
